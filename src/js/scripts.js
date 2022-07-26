a46ec73c23616fdfa50ce235417189976d93607b
Mikhail <litvinov.learn@gmail.com>
2022-07-26 17:45:36 +0200

    one_more_slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,22 +1,43 @@
 'use strict';
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left_solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right_solid.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-      });
+// slik-slider
+
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left_solid.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right_solid.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                   dots: true,
+//                   arrows: false
+//                 }
+//             }
+//         ]
+//       });
+//   });
+
+//tiny-slider
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.prev').addEventListener('click',  function () {
+    slider.goTo('prev');
   });
+
+document.querySelector('.next').addEventListener('click',  function () {
+    slider.goTo('next');    
+});
 
 
 
